b0b1ed992e01baf74f0a3f0382aeddf5ac81038e
Othman Andour <andour.otman@gmail.com>
2018-09-19 12:41:22 +0200

    Create 100k agents with all their attributes from the json file

--- model.py
+++ model.py
@@ -1,11 +1,14 @@
+import json
+
 class Agent:
     #This is how to define a method on a class
     def say_hello(self,first_name):
         return "Howdy "+first_name+" :D"
     #This is how to define an attribute on a class
-    def __init__(self,agreeableness):
-        self.agreeableness = agreeableness
+    def __init__(self,**agent_attributes):
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self,attr_name,attr_value)
 
-
-agent=Agent(2)
-print(agent.agreeableness)
+for agent_attributes in json.load(open("agents-100k.json")): #Open the json file and load it
+    agent = Agent(**agent_attributes)
+    print(agent.agreeableness)
