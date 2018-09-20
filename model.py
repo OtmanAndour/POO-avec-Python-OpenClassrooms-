5e4e15e49ba1094d8c7cd50096c2435495f63c26
Othman Andour <andour.otman@gmail.com>
2018-09-20 12:34:25 +0200

    added the population in the zones

--- model.py
+++ model.py
@@ -38,7 +38,14 @@ class Zone:
     def __init__(self,corner1,corner2):
         self.corner1=corner1
         self.corner2=corner2
-        self.inhabitants=0
+        self.inhabitants=[]
+
+    def add_inhabitant(self,inhabitant):
+        self.inhabitants.append(inhabitant)
+
+    @property
+    def population(self):
+        return len(self.inhabitants)
 
     @classmethod #This allows us to create an instance of the Zone class inside of the Zone class. We then need to replace all the self by cls
     def initialize_zones(cls):
@@ -48,7 +55,27 @@ class Zone:
                 top_right_corner=Position(longitude+cls.WIDTH_DEGREES,latitude+cls.HEIGHT_DEGREES)       
                 zone=Zone(bottom_left_corner,top_right_corner)
                 cls.ZONES.append(zone)
-        print (len(cls.ZONES))
+        
+
+    def contains(self, position):
+        return position.longitude >= min(self.corner1.longitude, self.corner2.longitude) and \
+            position.longitude < max(self.corner1.longitude, self.corner2.longitude) and \
+            position.latitude >= min(self.corner1.latitude, self.corner2.latitude) and \
+            position.latitude < max(self.corner1.latitude, self.corner2.latitude)
+            
+    @classmethod
+    def find_zone_that_contains(cls, position):
+        # Compute the index in the ZONES array that contains the given position
+        longitude_index = int((position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES)/ cls.WIDTH_DEGREES)
+        latitude_index = int((position.latitude_degrees - cls.MIN_LATITUDE_DEGREES)/ cls.HEIGHT_DEGREES)
+        longitude_bins = int((cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES) # 180-(-180) / 1
+        zone_index = latitude_index * longitude_bins + longitude_index
+
+        # Just checking that the index is correct
+        zone = cls.ZONES[zone_index]
+        assert zone.contains(position)
+
+        return zone
 
 def main ():
     for agent_attributes in json.load(open("agents-100k.json")): #Open the json file and load it
@@ -60,4 +87,7 @@ def main ():
         #Now we need to update the Agent class so that is gives a position to each agent created
         agent = Agent(position,**agent_attributes)
         Zone.initialize_zones()
+        zone=Zone.find_zone_that_contains(position)
+        zone.add_inhabitant(agent)
+        print(zone.population)
 main()
