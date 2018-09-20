81519b70ca9c297a23d1868336cb4807f6dff0f0
Othman Andour <andour.otman@gmail.com>
2018-09-20 11:33:39 +0200

    created the zone class to initialize all the zones

--- model.py
+++ model.py
@@ -25,6 +25,31 @@ class Position:
     def latitude(self):
         return self.latitude_degrees * (math.pi/180)
 
+class Zone:
+
+    ZONES=[]
+    MIN_LONGITUDE_DEGREES=-180
+    MAX_LONGITUDE_DEGREES=180
+    MIN_LATITUDE_DEGREES=-90
+    MAX_LATITUDE_DEGREES=90
+    HEIGHT_DEGREES=1
+    WIDTH_DEGREES=1
+
+    def __init__(self,corner1,corner2):
+        self.corner1=corner1
+        self.corner2=corner2
+        self.inhabitants=0
+
+    @classmethod #This allows us to create an instance of the Zone class inside of the Zone class. We then need to replace all the self by cls
+    def initialize_zones(cls):
+        for latitude in range(cls.MIN_LATITUDE_DEGREES,cls.MAX_LATITUDE_DEGREES,cls.HEIGHT_DEGREES):
+            for longitude in range(cls.MIN_LONGITUDE_DEGREES,cls.MAX_LONGITUDE_DEGREES,cls.WIDTH_DEGREES):
+                bottom_left_corner=Position(longitude,latitude)
+                top_right_corner=Position(longitude+cls.WIDTH_DEGREES,latitude+cls.HEIGHT_DEGREES)       
+                zone=Zone(bottom_left_corner,top_right_corner)
+                cls.ZONES.append(zone)
+        print (len(cls.ZONES))
+
 def main ():
     for agent_attributes in json.load(open("agents-100k.json")): #Open the json file and load it
         latitude=agent_attributes.pop('latitude')
@@ -34,6 +59,5 @@ def main ():
         #We then create an instance of the agent's position
         #Now we need to update the Agent class so that is gives a position to each agent created
         agent = Agent(position,**agent_attributes)
-        print(agent.position.latitude, agent.position.longitude)
-
+        Zone.initialize_zones()
 main()
